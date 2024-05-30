d58551c1e5bc4045d687076f0fcd8d7e98b5dbb5
Srinivas Hegde M <hegdesrinivasm@gmail.com>
2024-05-30 18:10:02 +0530

    Trying to develop a settings page.

---
+++ toura/lib/UI/settings_screen/settings_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SettingsList(
+        sections: [
+          SettingsSection(
+            title: const Text('Common'),
+            tiles: <SettingsTile>[
+              SettingsTile.navigation(
+                leading: const Icon(Icons.language),
+                title: const Text('Language'),
+                value: const Text('English'),
+              ),
+              SettingsTile.switchTile(
+                onToggle: (value) {},
+                initialValue: true,
+                leading: const Icon(Icons.format_paint),
+                title: const Text('Enable custom theme'),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
